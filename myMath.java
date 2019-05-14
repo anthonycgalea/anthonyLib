0fbb2e881a9ee0e1d914bf7b63d65fc456d8c910
anthonygalea3175 <anthonycgalea@gmail.com>
2019-05-14 17:07:40 -0400

    Added median function as well as a double arrayList sorting function

--- myMath.java
+++ myMath.java
@@ -42,6 +42,48 @@ public class myMath {
         return min;
     }
 
+    public static ArrayList<Double> sort(ArrayList<Double> list) {
+        ArrayList<Double> newList = new ArrayList<Double>();
+        if (list.size() < 2) {
+            return list;
+        }
+        newList.add(list.get(0));
+        for (int i = 1; i < list.size(); i++) {
+            boolean check = false;
+            for (int j = 0; j < newList.size(); j++) {
+                if (list.get(i) < newList.get(j) && check == false) {
+                    check = true;
+                    newList.add(j, list.get(i));
+                }
+            }
+            if (check == false) {
+                newList.add(list.get(i));
+            }
+        }
+
+        return newList;
+    }
+    
+    public static double findMedian(ArrayList<Double> numbers)  {
+        double med;
+        int medNum;
+        if (numbers.size() > 0) {
+            if (numbers.size()%2 == 1) {
+                medNum = numbers.size()/2;
+            }
+            else {
+                medNum = numbers.size()/2;
+            }
+            System.out.println(medNum);
+            med = sort(numbers).get(medNum);
+
+        }
+        else {
+            med = 0;
+        }
+        return med;
+     }
+
     public static double findMax(ArrayList<Double> numbers) {
         double max;
         if (numbers.size() > 0) {
@@ -70,12 +112,15 @@ public class myMath {
         double vari = variance(numbers);
         double min = findMin(numbers);
         double max = findMax(numbers);
+        double med = findMedian(numbers);
         System.out.println("Testing Functions:");
         System.out.println("Average:\t\t" + ave + "");
         System.out.println("Variance:\t\t" + vari + "");
         System.out.println("Standard Deviation:\t" + std + "");
         System.out.println("Minimum:\t\t" + min + "");
         System.out.println("Maximum:\t\t" + max + "");
+        System.out.println("Sorted List:\t\t" + sort(numbers));
+        System.out.println("Median:\t\t\t" + med);
         return;
     }
 
